60fdf272a07849ced00e7537d9694e0f9e776f6a
Michael Auderer <mike@silverstone.io>
2018-11-21 09:32:40 -0500

    add vscode, format dnf install command list

--- MOOBS.sh
+++ MOOBS.sh
@@ -8,12 +8,32 @@
 dnf install -y fedora-workstation-repositories
 dnf config-manager --set-enabled google-chrome
 
-# Add COPR repositories
+# Add VSCode key and repo
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+
+# Add repo for i3 and rofi
 dnf copr enable gregw/i3desktop
 
 # Install yummy packages
-dnf install -y i3-gaps rofi google-chrome-stable nano vim ranger w3m calcurse compton \
-feh arandr i3blocks i3lock rxvt-unicode scrot unar
+dnf install -y \
+	i3-gaps \
+	rofi \
+	google-chrome-stable \
+	code \
+	nano \
+	vim \
+	ranger \
+	w3m \
+	calcurse \
+	compton \
+	feh \
+	arandr \
+	i3blocks \
+	i3lock \
+	rxvt-unicode \
+	scrot \
+	unar
 
 # Install Discord
 flatpak install --from https://flathub.org/repo/appstream/com.discordapp.Discord.flatpakref 
